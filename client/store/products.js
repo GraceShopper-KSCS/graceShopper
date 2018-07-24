94fe79433c1f76d2abde000d28a988c356bb69db
Sivan Gilead <sivan.gilad@gmail.com>
2018-07-24 08:25:56 -0400

    add productlist component

---
+++ client/store/products.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+
+/**
+ * ACTION TYPES
+ */
+const GET_PRODUCTS = 'GET_PRODUCTS'
+
+
+/**
+ * INITIAL STATE
+ */
+const defaultProducts = {
+    products: []
+}
+
+/**
+ * ACTION CREATORS
+ */
+const getProducts = products => ({ type: GET_PRODUCTS, products })
+
+/**
+ * THUNK CREATORS
+ */
+export const loadProducts = () => async dispatch => {
+    try {
+        const res = await axios.get('/books')
+        dispatch(getProducts(res.data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+/**
+ * REDUCER
+ */
+export default function (state = defaultProduct, action) {
+    switch (action.type) {
+        case GET_PRODUCTS:
+            return action.products
+        default:
+            return state
+    }
+}
