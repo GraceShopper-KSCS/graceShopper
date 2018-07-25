47ba7823872ff5eaeaa7f6e6c27d7081c0955a4a
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-07-25 17:52:02 -0400

    Order/ProductOrder updated method

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,11 +1,29 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const ProductOrder = require('./productOrder')
 
 const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM,
     values: ['pending', 'processing', 'complete'],
     defaultValue: 'pending'
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
   }
 })
+Order.prototype.totalPrice = async function() {
+  const productorder = await ProductOrder.findAll({
+    where: {
+      orderid: this.id
+    }
+  })
+
+  if (productorder) {
+    const total = productorder.reduce((sum, qty) => sum + qty.unitprice, 0)
+    return total
+  }
+}
+
 module.exports = Order
