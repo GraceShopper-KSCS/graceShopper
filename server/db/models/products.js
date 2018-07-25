d2a4155cd82d044520d865470ec5922d20fadaae
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-07-25 16:08:21 -0400

    updated product/review model

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -1,31 +1,60 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const Review = require('./reviews')
 
 const Product = db.define('product', {
   title: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   author: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   description: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   price: {
     type: Sequelize.FLOAT,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   imageUrl: {
     type: Sequelize.STRING,
     defaultValue: 'https://image.flaticon.com/icons/svg/37/37631.svg'
   },
   inventory: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
   },
   category: {
     type: Sequelize.STRING
   }
 })
+Product.prototype.averageRating = async function() {
+  const reviews = await Review.findAll({
+    where: {productid: this.id}
+  })
+  if (reviews) {
+    const total = reviews.reduce((sum, review) => sum + review.rating, 0)
+    const length = reviews.length
+    return total / length
+  }
+}
 
 module.exports = Product
--- server/db/models/reviews.js
+++ server/db/models/reviews.js
@@ -1,8 +1,22 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
-const Review = db.define('review', {
-content:{
-  type:Sequelize.TEXT
-}
+const Review = db.define('reviews', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  rating: {
+    type: Sequelize.INTEGER
+  }
 })
-module.exports = Review;
+module.exports = Review
