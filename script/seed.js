ec6ca86a87a9d7d6805960a89ede4248290e9721
Kate Dubitski <kate.dubitski@gmail.com>
2018-07-28 23:37:07 -0400

    typo fix

--- script/seed.js
+++ script/seed.js
@@ -27,7 +27,7 @@ async function seed() {
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
   const users = await Promise.all([
-    User.create({email: '6', password: '123'}),
+    User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'}),
     User.create({email: 'admin@email.com', password: 'admin', admin: true})
   ])
