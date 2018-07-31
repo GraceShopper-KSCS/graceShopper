73d7e8096bab0b29d00ad69739ac57dcc32cd3a8
Claire Watson <watson.e.claire@gmail.com>
2018-07-31 15:46:41 -0400

    added submit order thunk

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const {Order, Product} = require('../db/models')
+const {Order, Product, ProductOrder} = require('../db/models')
+const {stripe} = require('../index')
 
 module.exports = router
 
@@ -14,6 +15,46 @@ router.get('/history', async (req, res, next) => {
     })
 
     res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/totalprice', async (req, res, next) => {
+  try {
+    const order = await Order.findAll({
+      where: {
+        userId: req.user.dataValues.id,
+        status: 'pending'
+      }
+    })
+
+    const totalPrices = await ProductOrder.findAll({
+      where: {orderId: order[0].dataValues.id}
+    })
+    let sum = 0
+    totalPrices.forEach(book => {
+      sum = book.totalPrice + sum
+    })
+
+    res.json(sum)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/', async (req, res, next) => {
+  try {
+    console.log('USER', req.user.dataValues.id)
+    const order = await Order.findOne({
+      where: {
+        userId: req.user.id,
+        status: 'pending'
+      }
+    })
+    const updatedOrder = await order.update({status: 'processing'})
+
+    res.json(updatedOrder)
   } catch (err) {
     next(err)
   }
