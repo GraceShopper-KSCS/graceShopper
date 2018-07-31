79538df3f9d3836e2b9a49f6285886fcc4d14ddc
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-07-31 15:09:49 -0400

    redux test

---
+++ client/components/quatity.spec.js
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import {expect} from 'chai'
+// // import enzyme, {shallow} from 'enzyme'
+// // import Adapter from 'enzyme-adapter-react-16'
+// import {IncrementItem, DecreaseItem} from './quantity'
+
+// global.expect = expect
+
+// describe('Local State', () => {
+//   it('should increment the counter in state', () => {
+//     const state = {counter: 0}
+//     const newState = IncrementItem(state)
+
+//     expect(newState.counter).to.equal(1)
+//   })
+
+//   it('should decrement the counter in state', () => {
+//     const state = {counter: 0}
+//     const newState = DecreaseItem(state)
+
+//     expect(newState.counter).to.equal(-1)
+//   })
+// })
